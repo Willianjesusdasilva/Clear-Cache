aee3bd01f8690f7cb590d0cbf848171dc50205a6
Willian Jesus da Silva <46569232+Willianjesusdasilva@users.noreply.github.com>
2021-03-16 15:47:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,17 @@
-import os 
-import win32con, win32api
+import os
 
-list = ['C:\Windows\Prefetch','C:\Windows\Temp']
+list_path = ['C:\Windows\Prefetch','C:\Windows\Temp', os.getenv('temp')]
 
 def clear_data(locate):
 	for raiz, diretorios, arquivos in os.walk(locate):
 		for arquivo in arquivos:
 			try:
-				print(arquivo)
-				win32api.SetFileAttributes(os.path.join(raiz,arquivo), win32con.FILE_ATTRIBUTE_NORMAL)
-				os.remove(os.path.join(raiz,arquivo))
+				os.remove(os.path.join(raiz, arquivo))
 			except:
-				print(arquivo+' Erro')
+				print(arquivo + ' Erro')
 
 
-temp = os.getenv('temp')
-temp = temp.replace('Roaming','\Local\Temp')
-list.append(temp)
-
-
-for i in list:
-	clear_data(i)
+for i in list_path:
+    clear_data(i)
 
 exit()
